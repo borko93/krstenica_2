4f545edb0c2469eb5dcd75aa6d502dff472190cd
Borko <borkokandic@gmail.com>
2024-05-24 12:14:10 +0200

    dodat message

--- krstenica/views.py
+++ krstenica/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import krsteni_list
 from .forms import krsteni_listForm
+from django.contrib import messages
 # Create your views here.
 
 def index(request):
@@ -8,6 +9,7 @@ def index(request):
         form = krsteni_listForm(request.POST)
         if form.is_valid():
             form.save()
+            messages.info(request, 'Крштени лист је сачуван!')
             return redirect('list')
     form = krsteni_listForm
 

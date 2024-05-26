7e7744ae6432f2da20f0c56feb61f984c0ace099
Borko <borkokandic@gmail.com>
2024-05-26 13:54:43 +0200

    choices added

--- krstenica/models.py
+++ krstenica/models.py
@@ -1,7 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
+
+
+DIJETE_BRACNO = (
+    ("Брачно", "Брачно"),
+    ("Брачно за грађанско правно подручје", "Брачно за грађанско правно подручје"),
+)
+
 # Create your models here.
+
 class krsteni_list(models.Model):
     knjiga = models.CharField(max_length=10)
     strana = models.CharField(max_length=1000)
@@ -18,7 +26,7 @@ class krsteni_list(models.Model):
     ime_prezime_i_pol = models.CharField(max_length=50)
     ime_prezime_zanimanje_roditelja = models.TextField()
     dijete_po_rođenju = models.CharField(max_length=10)
-    dijete_bračno = models.CharField(max_length=20)
+    dijete_bračno = models.CharField(max_length=50, choices=DIJETE_BRACNO)
     dijete_blizanac = models.CharField(max_length=10)
     dijete_mana = models.CharField(max_length=5)
     sveštenik = models.CharField(max_length=50)
@@ -32,6 +40,8 @@ class krsteni_list(models.Model):
 
     def __str__(self):
         return self.ime_prezime_i_pol
+
+
 
 
 
